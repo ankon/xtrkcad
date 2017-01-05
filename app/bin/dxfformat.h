e5c27a979d81c7961eacd916b61c88d11e60d102
Martin Fischer <m_fischer@users.sf.net>
2017-01-05 17:24:19 +0100

    All: Refactor DXF export including unittests

---
+++ app/bin/dxfformat.h
@@ -0,0 +1,18 @@
+#ifndef HAVE_DXFFORMAT_H
+#define HAVE_DXFFORMAT_H
+
+void DxfLayerName(DynString *result, char *name, int layer);
+void DxfFormatPosition(DynString *result, int type, double value);
+void DxfLineStyle(DynString *result, int isDashed);
+
+void DxfLineCommand(DynString *result, int layer, double x0, double yo, double x1, double y1, int style);
+void DxfCircleCommand(DynString *result, int layer, double x, double y, double r, int style);
+void DxfArcCommand(DynString *result, int layer, double x, double y, double r, double a0, double a1, int style);
+void DxfTextCommand(DynString *result, int layer, double x, double y, double size, char *text);
+
+void DxfPrologue(DynString *result, int layerCount, double x0, double y0, double x1, double y1);
+void DxfEpilogue(DynString *result);
+#define DXF_INDENT "  "
+
+#endif // !HAVE_DXFFORMAT_H
+
--- app/bin/dxfoutput.c
+++ app/bin/dxfoutput.c
@@ -20,7 +20,6 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
@@ -32,191 +31,198 @@
 #endif
 #endif
 
+#include <xtrkcad-config.h>
 #include <locale.h>
 #include <assert.h>
 #include "track.h"
 #include "i18n.h"
+#include <dynstring.h>
 
-static FILE * dxfF;
+#include "dxfformat.h"
+
 static struct wFilSel_t * exportDXFFile_fs;
 
 static void DxfLine(
-		drawCmd_p d,
-		coOrd p0,
-		coOrd p1,
-		wDrawWidth width,
-		wDrawColor color )
+    drawCmd_p d,
+    coOrd p0,
+    coOrd p1,
+    wDrawWidth width,
+    wDrawColor color)
 {
-	fprintf(dxfF, "  0\nLINE\n" );
-	fprintf(dxfF, "  8\n%s%d\n", sProdNameUpper, curTrackLayer+1 );
-	fprintf(dxfF, "  10\n%0.6f\n  20\n%0.6f\n  11\n%0.6f\n  21\n%0.6f\n",
-		p0.x, p0.y, p1.x, p1.y );
-	fprintf(dxfF, "  6\n%s\n", (d->options&DC_DASH)?"DASHED":"CONTINUOUS" );
+    DynString command = NaS;
+    DynStringMalloc(&command, 100);
+    DxfLineCommand(&command,
+                   curTrackLayer + 1,
+                   p0.x, p0.y,
+                   p1.x, p1.y,
+                   ((d->options&DC_DASH) != 0));
+    fputs(DynStringToCStr(&command), (FILE *)d->d);
+    DynStringFree(&command);
 }
 
 static void DxfArc(
-		drawCmd_p d,
-		coOrd p,
-		DIST_T r,
-		ANGLE_T angle0,
-		ANGLE_T angle1,
-		BOOL_T drawCenter,
-		wDrawWidth width,
-		wDrawColor color )
+    drawCmd_p d,
+    coOrd p,
+    DIST_T r,
+    ANGLE_T angle0,
+    ANGLE_T angle1,
+    BOOL_T drawCenter,
+    wDrawWidth width,
+    wDrawColor color)
 {
-	angle0 = NormalizeAngle(90.0-(angle0+angle1));
-	if (angle1 >= 360.0) {
-		fprintf(dxfF, "  0\nCIRCLE\n" );
-		fprintf(dxfF, "  10\n%0.6f\n  20\n%0.6f\n  40\n%0.6f\n",
-				p.x, p.y, r );
-	} else {
-		fprintf(dxfF, "  0\nARC\n" );
-		fprintf(dxfF, "  10\n%0.6f\n  20\n%0.6f\n  40\n%0.6f\n  50\n%0.6f\n  51\n%0.6f\n",
-				p.x, p.y, r, angle0, angle0+angle1 );
-	}
-	fprintf(dxfF, "  8\n%s%d\n", sProdNameUpper, curTrackLayer+1 );
-	fprintf(dxfF, "  6\n%s\n", (d->options&DC_DASH)?"DASHED":"CONTINUOUS" );
+    DynString command = NaS;
+    DynStringMalloc(&command, 100);
+    angle0 = NormalizeAngle(90.0-(angle0+angle1));
+
+    if (angle1 >= 360.0) {
+        DxfCircleCommand(&command,
+                         curTrackLayer + 1,
+                         p.x,
+                         p.y,
+                         r,
+                         ((d->options&DC_DASH) != 0));
+    } else {
+        DxfArcCommand(&command,
+                      curTrackLayer + 1,
+                      p.x,
+                      p.y,
+                      r,
+                      angle0,
+                      angle1,
+                      ((d->options&DC_DASH) != 0));
+    }
+
+    fputs(DynStringToCStr(&command), (FILE *)d->d);
+    DynStringFree(&command);
 }
 
 static void DxfString(
-		drawCmd_p d,
-		coOrd p,
-		ANGLE_T a,
-		char * s,
-		wFont_p fp,
-		FONTSIZE_T fontSize,
-		wDrawColor color )
+    drawCmd_p d,
+    coOrd p,
+    ANGLE_T a,
+    char * s,
+    wFont_p fp,
+    FONTSIZE_T fontSize,
+    wDrawColor color)
 {
-	fprintf(dxfF, "  0\nTEXT\n" );
-	fprintf(dxfF, "  1\n%s\n", s );
-	fprintf(dxfF, "  8\n%s%d\n", sProdNameUpper, curTrackLayer+1 );
-	fprintf(dxfF, "  10\n%0.6f\n  20\n%0.6f\n", p.x, p.y );
-	fprintf(dxfF, "  40\n%0.6f\n", fontSize/72.0 );
+    DynString command = NaS;
+    DynStringMalloc(&command, 100);
+    DxfTextCommand(&command,
+                   curTrackLayer + 1,
+                   p.x,
+                   p.y,
+                   fontSize,
+                   s);
+    fputs(DynStringToCStr(&command), (FILE *)d->d);
+    DynStringFree(&command);
 }
 
 static void DxfBitMap(
-		drawCmd_p d,
-		coOrd p,
-		wDrawBitMap_p bm,
-		wDrawColor color )
+    drawCmd_p d,
+    coOrd p,
+    wDrawBitMap_p bm,
+    wDrawColor color)
 {
 }
 
 static void DxfFillPoly(
-		drawCmd_p d,
-		int cnt,
-		coOrd * pts,
-		wDrawColor color )
+    drawCmd_p d,
+    int cnt,
+    coOrd * pts,
+    wDrawColor color)
 {
-	int inx;
-	for (inx=1; inx<cnt; inx++) {
-		DxfLine( d, pts[inx-1], pts[inx], 0, color );
-	}
-	DxfLine( d, pts[cnt-1], pts[0], 0, color );
+    int inx;
+
+    for (inx=1; inx<cnt; inx++) {
+        DxfLine(d, pts[inx-1], pts[inx], 0, color);
+    }
+
+    DxfLine(d, pts[cnt-1], pts[0], 0, color);
 }
 
-static void DxfFillCircle( drawCmd_p d, coOrd center, DIST_T radius, wDrawColor color )
+static void DxfFillCircle(drawCmd_p d, coOrd center, DIST_T radius,
+                          wDrawColor color)
 {
-	DxfArc( d, center, radius, 0.0, 360, FALSE, 0, color );
+    DxfArc(d, center, radius, 0.0, 360, FALSE, 0, color);
 }
 
 
 static drawFuncs_t dxfDrawFuncs = {
-		0,
-		DxfLine,
-		DxfArc,
-		DxfString,
-		DxfBitMap,
-		DxfFillPoly,
-		DxfFillCircle };
+    0,
+    DxfLine,
+    DxfArc,
+    DxfString,
+    DxfBitMap,
+    DxfFillPoly,
+    DxfFillCircle
+};
 
 static drawCmd_t dxfD = {
-		NULL, &dxfDrawFuncs, 0, 1.0, 0.0, {0.0,0.0}, {0.0,0.0}, Pix2CoOrd, CoOrd2Pix, 100.0 };
+    NULL, &dxfDrawFuncs, 0, 1.0, 0.0, {0.0,0.0}, {0.0,0.0}, Pix2CoOrd, CoOrd2Pix, 100.0
+};
 
 static int DoExportDXFTracks(
-		int cnt,
-		char ** fileName,
-		void * data )
+    int cnt,
+    char ** fileName,
+    void * data)
 {
-	time_t clock;
-	char *oldLocale;
+    time_t clock;
+    char *oldLocale;
+	DynString command = NaS;
+	FILE * dxfF;
 
-	assert( fileName != NULL );
-	assert( cnt == 1 );
+    assert(fileName != NULL);
+    assert(cnt == 1);
 
-	SetCurrentPath( DXFPATHKEY, fileName[ 0 ] );
-	dxfF = fopen( fileName[0], "w" );
-	if (dxfF==NULL) {
-		NoticeMessage( MSG_OPEN_FAIL, _("Continue"), NULL, "DXF", fileName[0], strerror(errno) );
-		return FALSE;
-	}
+	DynStringMalloc(&command, 100);
 
-	oldLocale = SaveLocale( "C" );
-	wSetCursor( wCursorWait );
-	time(&clock);
-	fprintf(dxfF,"\
-  0\nSECTION\n\
-  2\nHEADER\n\
-  9\n$ACADVER\n  1\nAC1009\n\
-  9\n$EXTMIN\n  10\n%0.6f\n  20\n%0.6f\n\
-  9\n$EXTMAX\n  10\n%0.6f\n  20\n%0.6f\n\
-  9\n$TEXTSTYLE\n  7\nSTANDARD\n\
-  0\nENDSEC\n\
-  0\nSECTION\n\
-  2\nTABLES\n\
-  0\nTABLE\n\
-  2\nLTYPE\n\
-  0\nLTYPE\n  2\nCONTINUOUS\n  70\n0\n\
-  3\nSolid line\n\
-  72\n65\n  73\n0\n  40\n0\n\
-  0\nLTYPE\n  2\nDASHED\n  70\n0\n\
-  3\n__ __ __ __ __ __ __ __ __ __ __ __ __ __ __\n\
-  72\n65\n  73\n2\n  40\n0.15\n  49\n0.1\n  49\n-0.05\n\
-  0\nLTYPE\n  2\nDOT\n  70\n0\n\
-  3\n...............................................\n\
-  72\n65\n  73\n2\n  40\n0.1\n  49\n0\n  49\n-0.05\n\
-  0\nENDTAB\n\
-  0\nTABLE\n\
-  2\nLAYER\n\
-  70\n0\n\
-  0\nLAYER\n  2\n%s1\n  6\nCONTINUOUS\n  62\n7\n  70\n0\n\
-  0\nLAYER\n  2\n%s2\n  6\nCONTINUOUS\n  62\n7\n  70\n0\n\
-  0\nLAYER\n  2\n%s3\n  6\nCONTINUOUS\n  62\n7\n  70\n0\n\
-  0\nLAYER\n  2\n%s4\n  6\nCONTINUOUS\n  62\n7\n  70\n0\n\
-  0\nLAYER\n  2\n%s5\n  6\nCONTINUOUS\n  62\n7\n  70\n0\n\
-  0\nLAYER\n  2\n%s6\n  6\nCONTINUOUS\n  62\n7\n  70\n0\n\
-  0\nLAYER\n  2\n%s7\n  6\nCONTINUOUS\n  62\n7\n  70\n0\n\
-  0\nLAYER\n  2\n%s8\n  6\nCONTINUOUS\n  62\n7\n  70\n0\n\
-  0\nLAYER\n  2\n%s9\n  6\nCONTINUOUS\n  62\n7\n  70\n0\n\
-  0\nLAYER\n  2\n%s10\n  6\nCONTINUOUS\n  62\n7\n  70\n0\n\
-  0\nENDTAB\n\
-  0\nENDSEC\n\
-  0\nSECTION\n\
-  2\nENTITIES\n\
-",
-		0.0, 0.0, mapD.size.x, mapD.size.y,
-		sProdNameUpper, sProdNameUpper, sProdNameUpper, sProdNameUpper, sProdNameUpper,
-		sProdNameUpper, sProdNameUpper, sProdNameUpper, sProdNameUpper, sProdNameUpper );
-	DrawSelectedTracks( &dxfD );
-	fprintf(dxfF,"  0\nENDSEC\n");
-	fprintf(dxfF,"  0\nEOF\n");
-	fclose(dxfF);
-	RestoreLocale( oldLocale );
-	Reset();
-	wSetCursor( wCursorNormal );
-	return TRUE;
+	SetCurrentPath(DXFPATHKEY, fileName[ 0 ]);
+    dxfF = fopen(fileName[0], "w");
+
+    if (dxfF==NULL) {
+        NoticeMessage(MSG_OPEN_FAIL, _("Continue"), NULL, "DXF", fileName[0],
+                      strerror(errno));
+        return FALSE;
+    }
+
+    oldLocale = SaveLocale("C");
+    wSetCursor(wCursorWait);
+    time(&clock);
+ 
+	DxfPrologue(&command, 10, 0.0, 0.0, mapD.size.x, mapD.size.y);
+	fputs(DynStringToCStr(&command), dxfF);
+	dxfD.d = (wDraw_p)dxfF;
+
+    DrawSelectedTracks(&dxfD);
+
+	DynStringClear(&command);
+	DxfEpilogue(&command);
+	fputs(DynStringToCStr(&command), dxfF);
+
+    fclose(dxfF);
+    RestoreLocale(oldLocale);
+    Reset();
+    wSetCursor(wCursorNormal);
+    return TRUE;
+}
+
+/**
+* Create and show the dialog for selected the DXF export filename
+*/
+
+void DoExportDXF(void)
+{
+    //if (selectedTrackCount <= 0) {
+    //    ErrorMessage(MSG_NO_SELECTED_TRK);
+    //    return;
+    //}
+    assert(selectedTrackCount > 0);
+
+    if (exportDXFFile_fs == NULL)
+        exportDXFFile_fs = wFilSelCreate(mainW, FS_SAVE, 0, _("Export to DXF"),
+                                         sDXFFilePattern, DoExportDXFTracks, NULL);
+
+    wFilSelect(exportDXFFile_fs, curDirName);
 }
 
 
-void DoExportDXF( void )
-{
-	if (selectedTrackCount <= 0) {
-		ErrorMessage( MSG_NO_SELECTED_TRK );
-		return;
-	}
-	if (exportDXFFile_fs == NULL)
-		exportDXFFile_fs = wFilSelCreate( mainW, FS_SAVE, 0, _("Export to DXF"),
-				sDXFFilePattern, DoExportDXFTracks, NULL );
-
-	wFilSelect( exportDXFFile_fs, curDirName );
-}
