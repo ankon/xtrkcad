7b7de2e588b78ff7a822f784dc53b9f805592ab7
Adam Richards <tynewydd@pacbell.net>
2018-08-27 00:21:32 -0700

    Fix issue with saved switch motor on restart causing abend.

--- app/bin/cswitchmotor.c
+++ app/bin/cswitchmotor.c
@@ -427,10 +427,10 @@ static void ReadSwitchMotor ( char * line )
 	xx->reverse = reverse;
 	xx->pointsense = pointsense;
     xx->turnindx = trkindex;
-    if (!last_motor) {
+    if (last_motor) {
     	last_trk = last_motor;
-//    	xx1 = GetswitchmotorData(last_trk);
-//    	xx1->next_motor = trk;
+    	xx1 = GetswitchmotorData(last_trk);
+    	xx1->next_motor = trk;
     } else first_motor = trk;
     xx->next_motor = NULL;
     last_motor = trk;
